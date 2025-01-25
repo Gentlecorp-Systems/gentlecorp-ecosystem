55dfbe3f70db69d9f288650abb6941a6258e6209
Caleb-Script <caleb-script@outlook.de>
2025-01-25 15:18:23 +0100

    remove CI/CD

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,18 @@
+// Datei: app/api/auth/[...nextauth]/route.ts
+
 import NextAuth from 'next-auth';
 import { authOptions } from '../../../../lib/authOptions';
 
-export const GET = NextAuth(authOptions);
-export const POST = NextAuth(authOptions);
+/**
+ * Konfiguriert die NextAuth-API-Routen.
+ * 
+ * - GET: Authentifizierungsdaten abrufen.
+ * - POST: Authentifizierungsaktionen ausführen.
+ */
+export const GET = async (request: Request) => {
+    return NextAuth(authOptions)(request);
+};
+
+export const POST = async (request: Request) => {
+    return NextAuth(authOptions)(request);
+};
